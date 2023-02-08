60784d525d89876c04bacc71197ed24a4851ec8b
JKearns3 <C21383126@mytudublin.ie>
2023-02-08 17:01:25 +0000

    Saved

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -10,6 +10,7 @@ public class Loops extends PApplet {
 	}
 
 	public void setup() {
+		colorMode(HSB, 360, 100, 100); 
 	}
 
 
@@ -23,8 +24,42 @@ public class Loops extends PApplet {
 	public void draw() {
 		background(0);
 		fill(255);
-		
-		
+
+		switch(mode)
+		{
+			case 0:
+			{
+				if(mouseX > width/4 && mouseX < width/2)
+				{
+					fill(180, 100, 100);
+					rect(width/4, 0, width/4, height);
+				}		
+				else if(mouseX > width/2 && mouseX < (width*.75))
+				{
+					fill(180, 100, 100);
+					rect(width/2, 0, width/4, height);
+				}
+				break;
+			}//end 0
+			case 1:
+			{
+				if(mouseX > width/4 && mouseX < width/2 && mouseY < height/2)
+				{
+					fill(180, 100, 100);
+					rect(width/4, 0, width/4, height/2);
+				}		
+				else if(mouseX > width/2 && mouseX < (width*.75) && mouseY < height/2)
+				{
+					fill(180, 100, 100);
+					rect(width/2, 0, width/4, height/2);
+				}
+				break;
+
+			}//end 1
+
+
+		}
+
 		
 
 	}
