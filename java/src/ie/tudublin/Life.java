f6129e87014e150d173c42d1b307176de52cd16d
JKearns3 <C21383126@mytudublin.ie>
2023-03-08 16:58:27 +0000

    randomise and clear options added

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -5,6 +5,8 @@ import processing.core.PApplet;
 public class Life extends PApplet
 {
 
+	int mode = 0;
+
 	LifeBoard board;
 	public void settings()
 	{
@@ -18,6 +20,36 @@ public class Life extends PApplet
 		board.randomise();
 	}
 
+	public void keyPressed()
+	{
+		if(key>= '0' && key <= '9')
+		{
+			mode = key - '0';
+		}
+
+		switch(mode)
+		{
+			case 1:
+			{
+				board.randomise();
+				break;
+			}
+			case 2:
+			{
+				for(int row = 0 ; row < 50 ; row ++)
+				{
+					for (int col = 0 ; col < 50 ; col ++)
+					{
+						board.board[row][col] = false;
+					}
+				}
+				break;
+			}
+
+		}
+		
+	}
+
 	public void draw()
 	{	
 		background(0);
@@ -25,4 +57,5 @@ public class Life extends PApplet
 		board.applyRules();
 		
 	}
+
 }
