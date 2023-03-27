bd70c9ead2ddc962ef785cdd483816608df482e1
JKearns3 <C21383126@mytudublin.ie>
2023-03-27 11:42:35 +0100

    fix

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -65,12 +65,7 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
-<<<<<<< HEAD
-		//audio1();
-		starMap();
-=======
 		life();
->>>>>>> 44b46f78b67684c08ae612f4dc0d0d719be21c5d
 	}
 	
 }
